2638721efda5ec441336b18d874b28a0907bebb9
Damian Babiarz <babiarz_damian@wp.pl>
2022-07-18 13:44:26 +0200

    feat: add jsconfig file

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Footer from '../components/Footer';
-import Navbar from '../components/Navbar';
-import Newsletter from '../components/Newsletter';
-import ResourceHighlight from '../components/ResourceHighlight';
-import ResourceList from '../components/ResourceList';
+import Footer from 'components/Footer';
+import Navbar from 'components/Navbar';
+import Newsletter from 'components/Newsletter';
+import ResourceHighlight from 'components/ResourceHighlight';
+import ResourceList from 'components/ResourceList';
 
 const Home = () => {
   return (
