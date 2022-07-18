fba65ae1c805030d37bee2e6c12a96ecf6f5aaa6
Damian Babiarz <babiarz_damian@wp.pl>
2022-07-18 13:58:07 +0200

    feat: add base data api structure

---
+++ api/data.js
@@ -0,0 +1,35 @@
+export const resources = [
+  {
+    id: 1,
+    title: 'Resource title',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores at neque cude!',
+    link: 'https://link.com',
+    image: 'https://image.com',
+    priority: 3,
+    timeToFinish: 120,
+    active: true,
+  },
+  {
+    id: 2,
+    title: 'Resource Nibh Sollicitudin',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores at neque cude!',
+    link: 'https://link.com',
+    image: 'https://image.com',
+    priority: 1,
+    timeToFinish: 60,
+    active: false,
+  },
+  {
+    id: 3,
+    title: 'Resource Sem Ornare',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores at neque cude!',
+    link: 'https://link.com',
+    image: 'https://image.com',
+    priority: 2,
+    timeToFinish: 30,
+    active: false,
+  },
+];
--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,15 @@ import Newsletter from 'components/Newsletter';
 import ResourceHighlight from 'components/ResourceHighlight';
 import ResourceList from 'components/ResourceList';
 
+import { resources } from 'api/data';
+
 const Home = () => {
   return (
     <Layout>
       <ResourceHighlight />
       <Newsletter />
       <ResourceList />
+      {JSON.stringify(resources)}
       <Footer />
     </Layout>
   );
