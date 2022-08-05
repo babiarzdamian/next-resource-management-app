6766f60045a7724f3c1010947822dc501deb2f3f
Damian Babiarz <babiarz_damian@wp.pl>
2022-08-05 09:12:02 +0200

    feat: add edit page base setup

---
+++ pages/resources/[id]/edit.js
@@ -0,0 +1,12 @@
+import Layout from 'components/Layout';
+import React from 'react';
+
+const ResourceEdit = () => {
+  return (
+    <Layout>
+      <div>ResourceEdit</div>
+    </Layout>
+  );
+};
+
+export default ResourceEdit;
